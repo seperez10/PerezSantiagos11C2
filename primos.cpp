f899c827a3d6539814e578698620dc8835910e35
seperez10 <se.perez10@uniandes.edu.co>
2019-04-09 11:12:10 -0500

    primos bien

--- primos.cpp
+++ primos.cpp
@@ -6,11 +6,12 @@ std::cout<<"Introdduzca x1"<<std::endl;
 std::cin>>x1;
 std::cout<<"Introdduzca x2"<<std::endl;
 std::cin>>x2;
-int contador=0;
+
 for (int i=x1;i<=x2;i++){
-	for (int j=1;j<=i;j++){
+int contador=0;
+	for (int j=2;j<i;j++){
 		if(i%j==0){contador++;}
-	if(contador==1){
+	if(contador==0){
 		std::cout<<i<<std::endl;
 		}
 }
